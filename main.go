ab5865ca309962756d7eaf4614a7459d1c493f78
Amit Gera <amitgera@Amits-MacBook-Pro.local>
2018-01-05 01:34:03 +0530

    v2

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-const version string = "1.0.0"
+const version string = "2.0.0"
 
 func main() {
 	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
